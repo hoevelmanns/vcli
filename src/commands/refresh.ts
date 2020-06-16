2561c9a6219ae497efca3f445300628f5cca0531
hoevelmanns <marcelle@hoevelmanns.io>
2020-06-16 02:23:07 +0200

    v0.8.8-poc

--- src/commands/refresh.ts
+++ src/commands/refresh.ts
@@ -1,6 +1,7 @@
 import { flags } from '@oclif/command';
 import Command from './base';
 import { Generator } from '../libs/generator/models';
+import { initProject } from "../libs/shared/utils";
 
 export default class Refresh extends Command {
     static hidden = false;
@@ -19,6 +20,7 @@ export default class Refresh extends Command {
 
     async run() {
         const { flags } = this.parse(Refresh);
-        new Generator(this.config.root).run(flags.vagrant);
+        initProject().then(() => new Generator(this.config.root).run(flags.vagrant))
+
     }
 }
